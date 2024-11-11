cd90d3fe368c2f6ad9c991afb150b97ec6d62df4
TraglerT <traglert@tutanota.com>
2024-11-11 18:57:07 +0100

    Code cleaning Documenting derm7pt_data

--- derm7pt_data.py
+++ derm7pt_data.py
@@ -6,17 +6,36 @@ import torch
 from torch.utils.data import Dataset
 from torchvision import transforms
 
+
 class Derm7pt_data(Dataset):
-    #Columns used in learning process for model
-    #Potential concept columns ['location', 'sex']
+    """
+    Custom dataset class for loading and transforming data for the Derm7pt model. This class loads image and metadata
+    information from a specified folder, applies data preprocessing, and groups specific diagnoses and dermatoscopic
+    features(concepts) together.
+
+    Attributes:
+        model_columns (dict): Dictionary specifying the columns for 'concepts' and 'label' used in the model.
+        diagnosis_mapping (DataFrame): Mapping table that categorizes specific diagnoses (labels) into more general diagnostic categories.
+        concepts_mapping (dict): Dictionary that maps different sub-concepts into general concepts and assigns them <name>_num = 1 if the concept is present.
+
+    Sub-Attributes:
+        <name>_num (Boolean): Indicates if concept or label present.
+        is_cancer (Boolean): Is label cancerous or not.
+        names (list of str): Corresponds to the values within the csv file that represent the type of label. Multiple names are grouped together.
+        abbrevs (str): a unique abbreviation that represents the label.
+        info (str): Helpful information. Not otherwise used.
+        <name>_score (int): 7-point criteria score given to that concept. Not used in the implementation.
+    """
+
+    # Column names for concepts and labels, used to extract data from metadata
     model_columns = {
         'concepts': ['pigment_network_num', 'streaks_num', 'pigmentation_num', 'regression_structures_num',
                      'dots_and_globules_num', 'blue_whitish_veil_num', 'vascular_structures_num'],
         'label': "diagnosis_num",
     }
 
-    ### zite: used code from Kawahara derm7pt Github as basis for this part (https://github.com/jeremykawahara/derm7pt) ###
-    #multiple specific class names grouped together by more general names
+    ### cite: used code from Kawahara derm7pt Github as basis for this part (https://github.com/jeremykawahara/derm7pt) ###
+    # multiple specific class names grouped together by more general names
     diagnosis_mapping: DataFrame = pd.DataFrame([
         {'diagnosis_num': 0, 'is_cancer': 1, 'names': 'basal cell carcinoma', 'abbrevs': 'BCC', 'info': 'Common non-melanoma cancer'},
         {'diagnosis_num': 1, 'is_cancer': 0,
@@ -32,7 +51,7 @@ class Derm7pt_data(Dataset):
         {'diagnosis_num': 4, 'is_cancer': 0, 'names': 'seborrheic keratosis', 'abbrevs': 'SK', 'info': 'benign'},
     ])
 
-    #Concepts (used grouped concepts for pigmentation, regression structures, and vascular structures)
+    # Concepts (used grouped concepts for pigmentation, regression structures, and vascular structures)
     concepts_mapping = {
         'pigment_network': pd.DataFrame([
             {'pigment_network_num': 0, 'names': ['absent', 'typical'], 'pigment_network_score': 0},
@@ -65,12 +84,18 @@ class Derm7pt_data(Dataset):
             {'vascular_structures_num': 1, 'names': ['dotted/irregular', 'dotted', 'linear irregular'], 'vascular_structures_score': 2},
         ]),
     }
-    ### end zite ###
+
+    ### end cite ###
 
     def __init__(self, data_folder: str):
+        """
+        Initialize the dataset with image and metadata paths and load metadata information from the given path.
+        Parameters:
+            data_folder (str): Directory containing images and metadata files.
+        """
         self.image_size = (768, 512)
         self.image_folder = os.path.join(os.path.normpath(data_folder), "images")
-        self.metadata = self.loadMeta(os.path.join(data_folder, "meta//meta.csv"))
+        self.metadata = self._loadMeta(os.path.join(data_folder, "meta//meta.csv"))
         if self.metadata.empty:
             self.labels = pd.DataFrame(columns=self.model_columns["label"])
         else:
@@ -79,17 +104,32 @@ class Derm7pt_data(Dataset):
             transforms.ToTensor(),
         ])
 
-    def __len__(self):
+    def __len__(self) -> int:
+        """
+        Return the number of records in the metadata (number of samples in the dataset).
+
+        Returns:
+            int: Number of samples in the dataset.
+        """
         return len(self.metadata)
 
+    def __getitem__(self, index: int) -> tuple:
+        """
+        Retrieve the image, label, and concepts for a specific index in the dataset.
 
-    def __getitem__(self, index):
+        Parameters:
+            index (int): Index of the item to retrieve.
+
+        Returns:
+            tuple: Tuple containing (image data as tensor, label, concepts as tensor).
+        """
         metadata = self.metadata.iloc[index]
 
         data = self.loadImage(metadata['derm'])
         # Convert the Pandas Series to a NumPy array and then to a PyTorch tensor
         data = self.transform(data)
 
+        # Retrieve label and concept values for the sample
         label = metadata[self.model_columns["label"]]
         concepts = pd.to_numeric(metadata[self.model_columns["concepts"]])
         concepts = torch.tensor(concepts.values, dtype=torch.float32)
@@ -98,10 +138,13 @@ class Derm7pt_data(Dataset):
 
     def loadImage(self, file: str):
         """
-        Load image and resize it to 768x512
+        Load image from file and resize it to self.image_size(768x512)
 
-        :param file:
-        :return: PixelImage
+        Parameters:
+            file (str): Filename of the image.
+
+        Returns:
+            PIL.Image.Image: Loaded and resized image.
         """
         file = os.path.normpath(file)
         path = os.path.join(self.image_folder, file)
@@ -109,31 +152,49 @@ class Derm7pt_data(Dataset):
         img = img.resize(self.image_size)
         return img
 
+    def _loadMeta(self, file_path: str) -> pd.DataFrame:
+        """
+        Load metadata from a CSV file, drop unnecessary columns, and merge specific diagnosis and concept mappings.
 
-    #load meta data
-    def loadMeta(self, file_path: str):
+        Parameters:
+            file_path (str): Path to the metadata CSV file.
+
+        Returns:
+            pd.DataFrame: DataFrame containing processed metadata.
+        """
         merged_df = pd.DataFrame()
         if os.path.exists(file_path):
             df = pd.read_csv(file_path)
-            #drop columns that are not needed
+            # drop columns that are not needed
             drop_columns = ['case_id', 'notes', 'management']
             df = df.drop(drop_columns, axis=1)
-            #diagnosis is very specific, so we merge it with the more general diagnosis grouping.
-            merged_df = df.merge(self.diagnosis_mapping.explode('names'), how='left', left_on='diagnosis', right_on='names')
+
+            # Merge specific diagnoses with the general diagnosis mapping
+            merged_df = df.merge(self.diagnosis_mapping.explode('names'), how='left', left_on='diagnosis',
+                                 right_on='names')
             merged_df = merged_df.drop('names', axis=1)
 
-            #concepts Merger
+            # Merge concept mappings
             for concept_name, mapping_df in self.concepts_mapping.items():
-                merged_df = merged_df.merge(mapping_df.explode('names'), how='left', left_on=concept_name, right_on='names')
+                merged_df = merged_df.merge(mapping_df.explode('names'), how='left', left_on=concept_name,
+                                            right_on='names')
                 merged_df = merged_df.drop('names', axis=1)
         return merged_df
 
-    # finding file for case sensitive operating systems. Since Folder and Filename are not always correct in meta file.
-    def __find_case_insensitive_path(self, path):
+    def __find_case_insensitive_path(self, path: str) -> str:
+        """
+        Find the correct case-sensitive path for systems with case-sensitive file systems.
+
+        Parameters:
+            path (str): Path to the file with case-insensitive naming.
+
+        Returns:
+            str: Correct case-sensitive file path if found.
+        """
         if os.path.isfile(path):
             return path
 
-        # check only last two parts of the path for case sensitivity
+        # Check case-sensitivity in the last two path elements
         parts = path.split(os.sep)
         current_path = os.path.join(os.sep, os.path.join(*parts[1:-2]))
         for part in parts[-2:]:
@@ -143,44 +204,3 @@ class Derm7pt_data(Dataset):
                     current_path = os.path.join(current_path, entry)
                     break
         return current_path
-
-
-
-
-#   multi_modal_concepts_mapping = {
-#         'pigment_network': pd.DataFrame([
-#             {'pigment_network_num': 0, 'names': 'absent', 'pigment_network_score': 0},
-#             {'pigment_network_num': 1, 'names': 'typical', 'pigment_network_score': 0},
-#             {'pigment_network_num': 2, 'names': 'atypical', 'pigment_network_score': 2},
-#         ]),
-#         'streaks': pd.DataFrame([
-#             {'streaks_num': 0, 'names': 'absent', 'streaks_score': 0},
-#             {'streaks_num': 1, 'names': 'regular', 'streaks_score': 0},
-#             {'streaks_num': 2, 'names': 'irregular', 'streaks_score': 1},
-#         ]),
-#         'pigmentation': pd.DataFrame([
-#             {'pigmentation_num': 0, 'names': 'absent', 'pigmentation_score': 0},
-#             {'pigmentation_num': 1, 'names': ['regular', 'diffuse regular', 'localized regular'], 'pigmentation_score': 0},
-#             {'pigmentation_num': 2, 'names': ['irregular', 'diffuse irregular', 'localized irregular'], 'pigmentation_score': 1},
-#         ]),
-#         'regression_structures': pd.DataFrame([
-#             {'regression_structures_num': 0, 'names': 'absent', 'regression_structures_score': 0},
-#             {'regression_structures_num': 1, 'names': ['present', 'blue areas', 'white areas', 'combinations'],
-#                 'regression_structures_score': 1},
-#         ]),
-#         'dots_and_globules': pd.DataFrame([
-#             {'dots_and_globules_num': 0, 'names': 'absent', 'dots_and_globules_score': 0},
-#             {'dots_and_globules_num': 1, 'names': 'regular', 'dots_and_globules_score': 0},
-#             {'dots_and_globules_num': 2, 'names': 'irregular', 'dots_and_globules_score': 1},
-#         ]),
-#         'blue_whitish_veil': pd.DataFrame([
-#             {'blue_whitish_veil_num': 0, 'names': 'absent', 'blue_whitish_veil_score': 0},
-#             {'blue_whitish_veil_num': 1, 'names': 'present', 'blue_whitish_veil_score': 2},
-#         ]),
-#         'vascular_structures': pd.DataFrame([
-#             {'vascular_structures_num': 0, 'names': 'absent', 'vascular_structures_score': 0},
-#             {'vascular_structures_num': 1, 'names': ['regular', 'arborizing', 'comma', 'hairpin',
-#                                                      'within regression', 'wreath'], 'vascular_structures_score': 0},
-#             {'vascular_structures_num': 2, 'names': ['dotted/irregular', 'dotted', 'linear irregular'], 'vascular_structures_score': 2},
-#         ]),
-#     }
